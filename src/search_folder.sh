cace2923fff8ebee5819019bdd73d4edd80df065
Jonathan Wells <jonwells0@gmail.com>
2016-05-05 12:57:46 +0100

    Add tests folder

--- src/search_folder.sh
+++ src/search_folder.sh
@@ -2,8 +2,8 @@
 
 for i in `ls *.fasta`; do
     base=$(echo $i | awk -F'.' '{print $1}')
-    hhblits -i "$base".fasta -d /usr/local/hh-suite/db/uniprot20_2016_02/uniprot20_2016_02 -oa3m "$base".a3m -n $1 -cpu 3 
+    hhblits -i "$base".fasta -d /usr/local/hh-suite/db/uniprot20_2016_02/uniprot20_2016_02 -oa3m "$base".a3m -n 2 -cpu $2
     addss.pl "$base".a3m "$base".a3m
-    hhsearch -i "$base".a3m -d $2 -cpu 3 $3
+    hhsearch -i "$base".a3m -d $1 -cpu $2
 done
 
